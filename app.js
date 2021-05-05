c4e7c8406adc67aa98742f52543c8f540bfe7398
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-05 16:42:49 -0300

    Começo de rotas separando o index do app

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const router = require('./routes/index')
 
-router.get('/', (req, res)=>{
-    res.send("Estamos no controle")
-})
+
 
 
 const app = express();
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const app = require ('./app')
+const mongoose = require('mongoose')
 
 
-app.set('port',7777);
- app.listen(app.get('port'), ()=>{
-    console.log("Servidor rodando na porta 7777")
+require('dotenv').config({path:'variables.env'})
+
+//Conexão com o banco de dados
+mongoose.connect(process.env.DATABASE,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.Promise = global.Promise;
+mongoose.connection.on("error", (error)=>{
+    console.error("ERRO: " + error.message)
 })
 
+
+
+app.set('port', process.env.PORT||7777);
+ const server = app.listen(app.get('port'), ()=>{
+    console.log("Servidor rodando na porta" + server.address().port)
+})
+ 
