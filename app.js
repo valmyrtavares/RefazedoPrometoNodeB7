66f10ec77148fbce88014eec746f1e7759fdd4e6
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-08 10:54:45 -0300

    testando mustache

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const mustache = require('mustache-express')
 const router = require('./routes/index')
 
 
-
-
+//configuração
 const app = express();
 app.use('/', router)
+
+app.engine('mst',mustache());
+app.set('view engine', 'mst');
+app.set('views', __dirname + '/views')
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 const express = require('express')
 const router = express.Router();
 
+
+
+
 router.get('/', (req, res)=>{
-    res.send("Estamos no completo")
+    const tipo = {
+        objt:{cor:"amarelo",tamanho:"Large",preco:"123"},
+        n:"Valmyr Tavares", 
+        idade:50,
+        nacionalidade:"Italiano",
+         array: [
+            {nome:"Carlos",idade:23}, 
+            {nome:"Leandro",idade:24},
+            {nome:"Fabricio",idade:34},
+        ],
+        outroArray:[
+            {cor:"amarelo",tamanho:"Large",preco:"123"},
+            {cor:"perto",tamanho:"middle",preco:"133"},
+            {cor:"verde",tamanho:"small",preco:"1200"},
+        ],
+        nomes:["Carla","Antonio"]
+    }
+    res.render('home', {tipo})
 })
 
-module.exports = router;
+router.get("/sobre",(req, res)=>{
+    const name ="Antonio Carlos"
+    res.send('menu nome é  ' + req.query.nome)
+})
+
+module.exports = router
