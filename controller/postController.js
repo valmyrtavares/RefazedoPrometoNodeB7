19190b2870c196a04b49d951f0f04e94ca1f4cee
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-11 16:18:45 -0300

    Voltando o erro

---
+++ controller/postController.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const Post = mongoose.model('Post')
+
+
+
+exports.add = (req, res)=>{
+    res.render('postAdd')
+};
---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+mongoose.Promise = global.Promise;
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        trim:true
+
+    },
+    slug:String,
+    body:{
+        type:String,
+        trim:true
+    },
+    tags:[String]
+});
+
+module.exports = mongoose.model('Post', postSchema)
