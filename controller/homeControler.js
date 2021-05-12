7aed871cbae626f58db4a7f59d6d8a0cf2edf69d
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-12 18:14:27 -0300

    listando posts aula 7 Fácil

---
+++ controller/homeControler.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Post = mongoose.model("Post")
+
+exports.index = async(req, res)=>{
+
+    let responseJson = {
+        posts:[]
+    }
+    const posts = await Post.find();
+    responseJson.posts = posts
+    res.render('home',responseJson)
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const postController = require('../controller/postController')
+const homeController = require('../controller/homeControler')
 
 const router = express.Router();
 
+router.get('/', homeController.index)
 
 router.get('/post/add', postController.add)
 router.post('/post/add',postController.addAction)
