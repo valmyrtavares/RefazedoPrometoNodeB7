5984f066aaec8590b9a82f06ad66e55199591398
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-05-19 16:48:19 -0300

    Refatorando o código do Post aula 23

--- models/Post.js
+++ models/Post.js
@@ -19,7 +19,10 @@ const postSchema = new mongoose.Schema({
         trim:true
     },
     tags:[String],
-    author:ObjectId 
+    author:{
+        type:ObjectId,
+        ref:'User'
+    }
 });
 
 postSchema.pre('save', async function(next){
@@ -47,21 +50,22 @@ postSchema.pre('save', async function(next){
     }
 
     postSchema.statics.findPosts = function(filters={}){
-        return this.aggregate([
-            {$match:filters},
-            {$lookup:{
-                from:'users',
-                let:{'author':'$author'},
-                pipeline:[ //Estava pipeLine e é pipeline dizia que tinha que  o argumento tinha que ser uma string
-                    {$match:{$expr:{$eq:[ '$$author' , '$_id' ] } } },
-                    {$limit:1}
-                ],
-                as:'author'
-            }},
-            {$addFields:{
-                'author':{ $arrayElemAt:[ '$author', 0 ] }
-            } }
-        ]);
+        return this.find(filters).populate('author')
+        // return this.aggregate([
+        //     {$match:filters},
+        //     {$lookup:{
+        //         from:'users',
+        //         let:{'author':'$author'},
+        //         pipeline:[ //Estava pipeLine e é pipeline dizia que tinha que  o argumento tinha que ser uma string
+        //             {$match:{$expr:{$eq:[ '$$author' , '$_id' ] } } },
+        //             {$limit:1}
+        //         ],
+        //         as:'author'
+        //     }},
+        //     {$addFields:{
+        //         'author':{ $arrayElemAt:[ '$author', 0 ] }
+        //     } }
+        // ]);
     }
 
 
